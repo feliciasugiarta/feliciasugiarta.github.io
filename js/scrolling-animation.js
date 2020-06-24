2b4b194e78ff1d801497908c46168768a1ca99a7
feliciasugiarta <feliciasugiarta@gmail.com>
2020-06-23 21:50:04 -0700

    Apply Code Kit

--- js/scrolling-animation.js
+++
@@ -1,7 +0,0 @@
-var $sun = $('.sun');
-var $win = $(window);
-
-$win.on('scroll', function () {
-  var top = $win.scrollTop() / 3;
-  $sun.css('transform', 'rotate(' + top + 'deg)');
-});
--- js/waypoints.js
+++
@@ -1,9 +0,0 @@
-var $dipper = $('.dipper');
-
-$dipper.waypoint(function (direction) {
-  if (direction == 'down') {
-    $dipper.addClass('js-comfort-animate');
-  } else {
-    $dipper.removeClass('js-comfort-animate');
-  }
-}, { offset: '50%' });
